ccf0e3b8ecfdd12c0c6503f3df432769e9eaae37
GiacomoMaletto <malettogiacomo6@gmail.com>
2022-09-05 16:28:34 +0200

    esponente

--- main.lua
+++ main.lua
@@ -16,13 +16,22 @@ local initial_x_shader = love.graphics.newShader([[
 ]])
 
 local initial_v_shader = love.graphics.newShader([[
+	uniform float seed;
+
+	float rand(vec2 n)
+	{
+		return fract(sin(dot(n + seed, vec2(12.9898, 4.1414))) * 43758.5453);
+	}
+
+    #define PI 3.1415926538
+	
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
 	{
-        vec4 v = normalize(vec4(1.0, 1.0, 1.0, 1.0));
-
-		return v;
+        float a = 2.0 * PI * rand(screen_coords);
+		return vec4(cos(a), sin(a), 0.0, 0.0);
 	}
 ]])
+initial_v_shader:send("seed", (os.time()%10000)/10000)
 
 local x_shader = love.graphics.newShader([[
     float f1(float x, float y){
@@ -32,8 +41,6 @@ local x_shader = love.graphics.newShader([[
     float f2(float x, float y){
         return sin(y)*pow(cos(x)+cos(y)+4.0, -2.0);
     }
-
-    #define PI 3.1415926538
 
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
 	{
@@ -84,37 +91,55 @@ local v_shader = love.graphics.newShader([[
 
         vec4 v = Texel(texture, texture_coords);
 
-        //return (normalize(J*v));
 	    return (J*v);
 	}
 ]])
 
 local mv_shader = love.graphics.newShader([[
 	uniform Image v_image;
+    uniform Image ve_image;
 
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
 	{
         vec4 v = Texel(v_image, texture_coords);
-        float nv = log(length(v));
+        float ve = Texel(ve_image, texture_coords)[0];
         float mv = Texel(texture, texture_coords)[0];
+        float nv = log(length(v));
 
-		return vec4(max(mv,nv), 0.0, 0.0, 1.0);
+		return vec4(max(mv,nv+ve), 0.0, 0.0, 0.0);
 	}
 ]])
 
 local display_shader = love.graphics.newShader([[
-    const float infinity = 1. / 0.;
-    float NaN = 0.0/0.0;
-    const float minf = -1.0/0.0;
-
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
 	{
         float x = texture_coords[0];
         float y = 1.0 - texture_coords[1];
         float mv = Texel(texture, vec2(x,y))[0];
 
-        float c = (10.0 - mv)/(10.0 - 1.5);
+        float c = (mv - 1.5)/(4.0 - 1.5);
 		return vec4(c, c, c, 1.0);
+	}
+]])
+
+local ve_shader = love.graphics.newShader([[
+    uniform Image v_image;
+
+	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
+	{
+        vec4 v = Texel(v_image, texture_coords);
+        float ve = Texel(texture, texture_coords)[0];
+        float nv = length(v);
+
+		return vec4(ve + log(nv), 0.0, 0.0, 0.0);
+	}
+]])
+
+local scale_shader = love.graphics.newShader([[
+	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
+	{
+        vec4 v = Texel(texture, texture_coords);
+		return normalize(v);
 	}
 ]])
 
@@ -152,14 +177,22 @@ love.graphics.draw(white)
 love.graphics.setShader()
 love.graphics.setCanvas()
 
+local ve = {}
+ve[1] = love.graphics.newCanvas(nw, nh, {format="r32f"})
+ve[2] = love.graphics.newCanvas(nw, nh, {format="r32f"})
+
 local mv = {}
 mv[1] = love.graphics.newCanvas(nw, nh, {format="r32f"})
 mv[2] = love.graphics.newCanvas(nw, nh, {format="r32f"})
 
+local t = 0
+local T = 100
 function love.update(dt)
     if love.keyboard.isDown("escape") then
         love.event.quit()
     end
+
+    t = t + 1
 end
 
 function love.draw()
@@ -175,13 +208,29 @@ function love.draw()
     love.graphics.setShader(mv_shader)
     love.graphics.setCanvas(mv[current])
     mv_shader:send("v_image", v[3-current])
+    mv_shader:send("ve_image", ve[3-current])
     love.graphics.draw(mv[3-current])
 
     love.graphics.setShader(display_shader)
     love.graphics.setCanvas()
     love.graphics.draw(mv[current])
 
-    love.graphics.setShader()
+    if t == T then
+        v[current], v[3-current] = v[3-current], v[current]
+
+        love.graphics.setShader(ve_shader)
+        love.graphics.setCanvas(ve[current])
+        ve_shader:send("v_image", v[3-current])
+        love.graphics.draw(ve[3-current])
+
+        love.graphics.setShader(scale_shader)
+        love.graphics.setCanvas(v[current])
+        love.graphics.draw(v[3-current])
+
+        t = 0
+    end
+
+    love.graphics.setCanvas()
 
     current = 3-current
 end
